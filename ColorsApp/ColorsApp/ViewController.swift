65af5104d737b7d40272485a99fcb3164365f280
Camila Maia <cmaiacd@gmail.com>
2018-05-30 18:31:23 -0300

    Initial view - static

---
+++ ColorsApp/ColorsApp/ViewController.swift
@@ -0,0 +1,35 @@
+//
+//  ViewController.swift
+//  ColorsApp
+//
+//  Created by Camila Maia on 30/05/18.
+//  Copyright © 2018 Camila Maia. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var sliderOutlet: UISlider!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        print("Hello World")
+    }
+
+    @IBAction func btnYellow(_ sender: Any) {
+    }
+    
+    @IBAction func btnBlue(_ sender: Any) {
+    }
+    
+    @IBAction func btnRed(_ sender: Any) {
+    }
+    
+    @IBAction func btnResetColors(_ sender: Any) {
+    }
+    
+    @IBAction func slider(_ sender: Any) {
+    }
+}
+
